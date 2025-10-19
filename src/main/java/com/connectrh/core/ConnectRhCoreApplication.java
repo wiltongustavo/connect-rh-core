9b0a430cc9efc84078929c5e54c1f7f653df21d6
wiltongustavo <wilton.gustavo.eu@hotmail.com>
2025-10-18 23:42:39 -0300

    first commit

---
+++ src/main/java/com/connectrh/core/ConnectRhCoreApplication.java
@@ -0,0 +1,13 @@
+package com.connectrh.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectRhCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectRhCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/connectrh/core/ConnectRhCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.connectrh.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectRhCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
